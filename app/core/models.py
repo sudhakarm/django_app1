3115eb5c9d3b9c324d637a5120557aedb65ae7d4
Sudhakar Mangipudi <sudhakar.mangipudi@gmail.com>
2023-03-31 00:24:23 +0530

    added k8 definitions

--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Users(models.Model):
+    """ class user """
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     createdon = models.DateTimeField(auto_now_add=True)
